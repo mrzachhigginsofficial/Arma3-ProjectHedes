4e399e481c336af42e617e22c233a64d1e643e31
mrzachhigginsofficial <53547287+mrzachhigginsofficial@users.noreply.github.com>
2021-02-07 04:54:01 -0500

    Outline

---
+++ src/HedesClientMod/config.cpp
@@ -0,0 +1,21 @@
+class CfgPatches
+{
+	class ProjectHedesClient
+	{
+        name = "Project Hedes Client";
+		author = "ZanchoElGrande";
+		url = "";
+		requiredVersion = 0.1;
+		fileName = "HEDES_Client.pbo";
+        units[] = {};
+		weapons[] = {};
+        requiredAddons[] = { };
+	};
+};
+
+class CfgFunctions 
+{
+    //#include "initializations\init_config.hpp"
+    //#include "globalvariables\globals_config.hpp"
+    //#include "functionslibrary\fl_config.hpp"
+};
---
+++ src/HedesServerMod/config.cpp
@@ -0,0 +1,21 @@
+class CfgPatches
+{
+	class ProjectHedesServer
+	{
+        name = "Project Hedes Server";
+		author = "ZanchoElGrande";
+		url = "";
+		requiredVersion = 0.1;
+		fileName = "HEDES_Server.pbo";
+        units[] = {};
+		weapons[] = {};
+        requiredAddons[] = { };
+	};
+};
+
+class CfgFunctions 
+{
+    //#include "initializations\init_config.hpp"
+    //#include "globalvariables\globals_config.hpp"
+    //#include "functionslibrary\fl_config.hpp"
+};
