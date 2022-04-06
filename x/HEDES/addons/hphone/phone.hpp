f3af12e8e5bf5291efa36b52ce370d6eab979844
Zach <zachhiggins@gmail.com>
2022-04-06 02:08:27 -0400

    basic phone interface started

--- x/HEDES/addons/hphone/phone.hpp
+++ x/HEDES/addons/hphone/phone.hpp
@@ -1,38 +1,73 @@
-////////////////////////////////////////////////////////
-// GUI EDITOR OUTPUT START (by zachh, v1.063, #Rivoso)
-////////////////////////////////////////////////////////
-
-/* #Rivoso
-$[
-	1.063,
-	["hphone",[[0,0,1,1],0.025,0.04,"GUI_GRID"],0,0,0],
-	[1600,"",[1,"OK",["0.396875 * safezoneW + safezoneX","0.522 * safezoneH + safezoneY","0.04125 * safezoneW","0.055 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
-	[1000,"",[1,"This is a test window.",["0.396875 * safezoneW + safezoneX","0.456 * safezoneH + safezoneY","0.2 * safezoneW","0.055 * safezoneH"],[0,0,0,1],[1,1,1,1],[-1,-1,-1,-1],"","-1"],[]]
-]
-*/
-
 import RscPicture;
+import RscStructuredText;
+import RscButton;
 
-class hphone
+class HEDES_hPhone
 {
-	idd = 1000;
 	enableSimulation = 1;
 	enableDisplay = 1;
 	movingEnable = true;
 
+	name="HEDES_hPhone";
+	onLoad = "with uiNameSpace do { HEDES_hPhone = _this select 0 }";
+
 	class Controls
 	{
-		class RscPicture_1602: RscPicture
+		class 1500: RscPicture
 		{
-			idc = 1602;
-			text = "\x\HEDES\addons\hphone\hphone.paa";
-			x = 0.3 * safezoneW + safezoneX;
-			y = 0.1 * safezoneH + safezoneY;
-			w = .3 * safezoneW;
-			h = .5 * safezoneH;
+			idc = 1500;
+			text = "\x\HEDES\addons\hphone\hphone_rugged_1024.paa";
+			x = 0.5 * safezoneW + safezoneX;
+			y = 0.35 * safezoneH + safezoneY;
+			w = .45 * safezoneW;
+			h = .70 * safezoneH;
+		};
+	}
+}
+
+class app_admintelemetry
+{
+	enableSimulation = 1;
+	enableDisplay = 1;
+	movingEnable = true;
+
+	name="HEDES_app_admintelemetry";
+	onLoad = "with uiNameSpace do { HEDES_app_admintelemetry = _this select 0 }";
+
+	class Controls
+	{
+		class RscButton_1601: RscButton
+		{
+			idc = 1601;
+			text = "CLOSE APP";
+			x = 0.6645 * safezoneW + safezoneX;
+			y = 0.780 * safezoneH + safezoneY;
+			w = .110 * safezoneW;
+			h = .02 * safezoneH;
+			sizeEx = .02;
+			font = "EtelkaMonospacePro";
+		};
+
+		class RscStructuredText_1603: RscStructuredText
+		{
+			idc = 1603;
+			type = 13;
+			text = "";
+			x = 0.6620 * safezoneW + safezoneX;
+			y = 0.485 * safezoneH + safezoneY;
+			w = .115 * safezoneW;
+			h = .32 * safezoneH;
+			size = .02;
+			colorBackground[] = { 1, 1, 1, .1 };
+			
+			class Attributes
+			{
+				font = "EtelkaMonospacePro";
+				color = "#ffffff";
+				colorLink = "#D09B43";
+				align = "left";
+				shadow = 1;
+			};
 		};
 	};
 };
-////////////////////////////////////////////////////////
-// GUI EDITOR OUTPUT END
-////////////////////////////////////////////////////////
