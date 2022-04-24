d632cd475441367ed437e50ad7e0dca754b26649
Zach <zachhiggins@gmail.com>
2022-04-24 04:52:16 -0400

    Airports? #19

--- x/HEDES/addons/airportmodules/CfgVehicles.hpp
+++ x/HEDES/addons/airportmodules/CfgVehicles.hpp
@@ -22,9 +22,8 @@ class CfgVehicles
 		};
 	};	
 
-	class GVAR(AirportModule_BASE) : Module_F
+	class GVAR(BASE) : Module_F
 	{
-		canSetArea=1;
 		category = QUOTE(GVAR(AirportModules));
 		functionPriority = 1;
 		isTriggerActivated = 0;
@@ -36,52 +35,37 @@ class CfgVehicles
 				property = QUOTE(GVAR(AirportModules_Units));
 			};
 
-			class UnitPool
+			class EditMulti3 
 			{
-				property = "HEDES_AirportModule_UnitPool";
-				control = "EditCodeMulti5";
-				displayName = "Array of unit types that will spawn.";
-				tooltip = "Array of cfgvehicles to spawn. Must be formatted as array: ['var1','var2'].";
-				defaultValue = """['B_Plane_CAS_01_dynamicLoadout_F','B_Plane_Fighter_01_F']""";
+				property = "HEDES_AirportModule_EditMulti3";
+				control = "EditCodeMulti3";				
+				expression = "_this setVariable ['%s',_value];";
+			}
+
+			class EditMulti5
+			{
+				property = "HEDES_AirportModule_EditMulti5";
+				control = "EditCodeMulti5";				
+				expression = "_this setVariable ['%s',_value];";
+			};			
+
+			class Slider
+			{
+				property = "HEDES_AirportModule_Slider";
+				control = "Slider";
 				expression = "_this setVariable ['%s',_value];";
 			};			
 
 			class UnitInit
 			{
 				property = "HEDES_AirportModule_UnitInit";
-				control = "EditCodeMulti5";
-				displayName = "Unit Init Function.";
-				tooltip = "Spawn unit passed as _this (group _this, vehicle _this, removebackpack _this, etc.).";
-				defaultValue = "_this setdamage 0";
+				control = "EditCodeMulti3";
 				expression = "_this setVariable ['%s',_value];";
 			};			
 
-			class WPRadius : Edit
+			class EditNumber : Edit
 			{
-				property = "HEDES_AirportModule_RPRadius";
-				displayName = "WP Radius.";
-				tooltip = "Make this bigger if the aircraft doesn't seem to be triggering waypoints.";
-				defaultValue = "100";
-				typeName = "NUMBER";
-				validate = "number";
-			};
-
-			class NumberOfUnits : Edit
-			{
-				property = "HEDES_AirportModule_NumOfUnits";
-				displayName = "Number of units/vehicles.";
-				tooltip = "Number of units/vehicles that can be in the area at one time.";
-				defaultValue = "3";
-				typeName = "NUMBER";
-				validate = "number";
-			};
-
-			class UnitTimeout : Edit
-			{
-				property = "HEDES_AirportModule_Timeout";
-				displayName = "Unit State Timeout.";
-				tooltip = "How long until a unit is considered bugged and deleted.";
-				defaultValue = "240";
+				property = "HEDES_AirportModule_EditNumber";
 				typeName = "NUMBER";
 				validate = "number";
 			};
@@ -91,7 +75,7 @@ class CfgVehicles
 				property = "HEDES_AirportModule_UnitSide";
 				displayName = "Units Side";
 				tooltip = "The Spawned Unit Side.";
-				defaultValue = "WEST";
+				defaultValue = """WEST""";
 				
 				class Values
 				{
@@ -109,6 +93,26 @@ class CfgVehicles
 					};
 				};
 			};
+
+			class MissionType : Combo
+			{
+				property = "HEDES_AirportModule_Mission";
+				displayName = "Units Mission";
+				tooltip = "The Spawned Units Mission.";
+				defaultValue = """Ambient""";
+				
+				class Values
+				{
+					class Ambient	{
+						name = "Ambient"; 
+						value = "Ambient";
+					};
+					class Patrol	{
+						name = "Patrol"; 
+						value = "Patrol";
+					};
+				};
+			};
 		};
 
 		class ModuleDescription: ModuleDescription
@@ -117,25 +121,91 @@ class CfgVehicles
 		};
 	};
 
-	class GVAR(AirportModule_Aircraft): GVAR(AirportModule_BASE)
+	class GVAR(Aircraft): GVAR(BASE)
 	{
+		canSetArea=0;
 		displayName = "Ambient Airplane Sim (Early Access)";
 		function = QUOTE(FUNCMAIN(InitAirportManager));
 		scope = 2;
 
 		class Attributes: Attributes
 		{
-			class NumberOfUnits : NumberOfUnits { };
-			class UnitSide : UnitSide { };
-			class UnitPool : UnitPool
-			{
-				displayName = "Array of unit types.";
-				tooltip = "Array of units. Must be formatted as array: ['var1','var2']. Types are from CfgVehicles. Only vehicles here, not infantry.";
-				defaultValue = """['B_Plane_CAS_01_dynamicLoadout_F','B_Plane_Fighter_01_F']""";
+			class NumberOfUnits : EditNumber {
+				property = "HEDES_AirportModule_NumOfUnits";
+				displayName = "Number of units/vehicles";
+				tooltip = "Number of units/vehicles that can be in the area at one time.";
+				defaultValue = "3";
 			};
-			class WPRadius : WPRadius { };
-			class UnitTimeout : UnitTimeout { };
-			class UnitInit : UnitInit { };			
+			class UnitSide : UnitSide { };
+			class MissionType : MissionType { };
+			class MaxFuel : Slider { 
+				property = "HEDES_AirportModule_MaxFuel";
+				displayName = "Max Fuel (when spawned/reset)";
+				tooltip = "How much fuel will be added to the plae (controls flight time).";
+				defaultValue = ".5";
+			};
+			class MinFuel : Slider {
+				property = "HEDES_AirportModule_MinFuel";
+				displayName = "Min Fuel (when recalled)";
+				tooltip = "How much fuel when plane is recalled to airport.";
+				defaultValue = ".25";
+			}
+			class UnitPool : EditMulti3 {
+				displayName = "Array of unit types";
+				defaultValue = """['B_Plane_CAS_01_dynamicLoadout_F','B_Plane_Fighter_01_F']""";
+				property = "HEDES_AirportModule_UnitPool";
+				tooltip = "Array of units. Must be formatted as array: ['var1','var2']. Types are from CfgVehicles. Only vehicles here, not infantry.";				
+			};
+			class WPRadius : EditNumber {
+				property = "HEDES_AirportModule_RPRadius";
+				displayName = "WP Radius";
+				tooltip = "Make this bigger if the aircraft doesn't seem to be triggering waypoints.";
+				defaultValue = "100";
+			};
+			class UnitTimeout : EditNumber {
+				property = "HEDES_AirportModule_Timeout";
+				displayName = "Unit State Timeout";
+				tooltip = "How long until a unit is considered bugged and deleted.";
+				defaultValue = "240";
+			};
+			class UnitInit : EditMulti5 {
+				property = "HEDES_AirportModule_UnitInit";
+				displayName = "Unit Init Function";
+				tooltip = "Expression called after new jet/group spawned. Script parameters, [_jet, _grp]: _jet = _this # 0; _grp = _this # 1;... or include params['_jet','_grp'].";
+			};			
 		};
+	};
+
+	class GVAR(Cleaner): GVAR(BASE)
+	{
+		canSetArea=1;
+		displayName = "Ambient Airport Cleaner (WIP)";
+		scope = 2;
+
+		class AttributeValues
+		{
+			size3[]={50,50,-1};
+			isRectangle=1;
+		};
+
+		class Attributes: Attributes { };
+	};
+
+	class GVAR(SpawnPoint): GVAR(BASE)
+	{
+		canSetArea=0;
+		displayName = "Ambient Airport Spawn Point (WIP)";
+		scope = 2;
+
+		class Attributes: Attributes { };
+	};
+
+	class GVAR(DespawnPoint): GVAR(BASE)
+	{
+		canSetArea=0;
+		displayName = "Ambient Airport Despawn Point (WIP)";
+		scope = 2;
+
+		class Attributes: Attributes { };
 	};
 };
